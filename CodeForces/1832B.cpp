c749c16cc75acbc7a2784256c36dcb35e2530510
Qual207 <103975082+Qual207@users.noreply.github.com>
2023-06-03 14:07:58 -0400

    Update 1832B.cpp

--- CodeForces/1832B.cpp
+++ CodeForces/1832B.cpp
@@ -7,6 +7,11 @@ how to come up with a non brute force solution since how would you know when to 
 I couldn't come up with the connection that you can only make two moves: remove maximum, or two minimums and it doesn't matter the order that you do these moves. 
 I didn't think to generalize the process because with each case, you just remove a certain amount of two minimums and a certain amount of maximums and you are left with a
 brute forcable middle section.
+
+Maybe a better thinking process: After trying the case: "6 2 - 15 22 12 10 13 11", we realize that greedy does not work, which means there has to be a good brute force
+approach rather than some specific solution. But, the most basic brute force approach is to try all 2^k combinations right? But after thinking about all these combinations 
+more, we realize that we can shorten this by noticing how removing 2 min and then 1 max is the same as removing 1 max and then 2 min which means that every single combination
+can be limited to how many times a maximum was taken away versus how many times "2 minimums" were taken away.
 */
 
 #include <bits/stdc++.h>
